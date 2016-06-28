3e46e09fa17c4edd4e906f5cae61c24b13cb9051
Edward Schults <edward.schults@gmail.com>
2016-06-28 18:42:35 +0200

    preparing boilerplate

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
+require_relative "config/application"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
-set :database, { adapter: "sqlite3", database: "db/database.sqlite3" }
+get "/" do
+  "Hello world!"
+end
