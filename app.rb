af01cb5674ba684139bf10fd588fec2d51f44ea2
Edward Schults <edward.schults@gmail.com>
2016-06-28 18:22:07 +0200

    updated db path

--- app.rb
+++ app.rb
@@ -2,4 +2,4 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
-set :database, { adapter: "sqlite3", database: "database.sqlite3" }
+set :database, { adapter: "sqlite3", database: "db/database.sqlite3" }
